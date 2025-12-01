989d9f5d6b22bae43794a304eca7f6fc76080651
Ryan Groch <ryan.w.groch@gmail.com>
2025-12-01 11:39:03 -0600

    Combines python solutions

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,41 +1,11 @@
 import sys
 
-rotations = []
+pos = 50
+password1 = 0
+password2 = 0
 
 with open(sys.argv[1], "r") as f:
-    for line in f:
-        rotations.append(line)
-
-
-def part1():
-    pos = 50
-    password = 0
-
-    for rot in rotations:
-        direction = rot[0]
-        amount = int(rot[1:])
-
-        if direction == "R":
-            pos += amount
-        else:
-            pos -= amount
-
-        while pos < 0:
-            pos += 100
-
-        pos %= 100
-
-        if pos == 0:
-            password += 1
-    
-    return password
-
-
-def part2():
-    pos = 50
-    password = 0
-
-    for rot in rotations:
+    for rot in f:
         direction = rot[0]
         amount = int(rot[1:])
         start_0 = pos == 0
@@ -46,24 +16,25 @@ def part2():
             pos -= amount
 
         if pos == 0:
-            password += 1
+            password2 += 1
 
         while pos < 0:
             pos += 100
 
             if not start_0:
-                password += 1
+                password2 += 1
             else:
                 start_0 = False
 
             if pos == 0:
-                password += 1
+                password2 += 1
 
         while pos > 99:
             pos -= 100
-            password += 1
+            password2 += 1
 
-    return password
+        if pos == 0:
+            password1 += 1
 
-print(f"Part 1: {part1()}")
-print(f"Part 2: {part2()}")
+print(f"Part 1: {password1}")
+print(f"Part 2: {password2}")
