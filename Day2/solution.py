7d23229310f4c1d366980891c87f9564c6f97e8e
Ryan Groch <ryan.w.groch@gmail.com>
2025-12-02 01:03:16 -0600

    Day 2 part 2

--- Day2/solution.py
+++ Day2/solution.py
@@ -5,13 +5,14 @@ input = Path(sys.argv[1]).read_text()
 ranges = input.split(",")
 
 part1_total = 0
+part2_total = 0
 
 for r in ranges:
-    # print(ranges[3])
     low,high = r.split("-")
     for id in range(int(low), int(high) + 1):
         id_str = str(id)
         valid = True
+        counted = False
 
         if len(id_str) == 1:
             continue
@@ -22,23 +23,22 @@ for r in ranges:
 
             pattern = id_str[:i]
 
-            if 2*len(pattern) != len(id_str):
-                continue
-
             repeats = True
             for j in range(i, len(id_str), len(pattern)):
                 subsection = id_str[j:j+len(pattern)]
-                # print(id_str, pattern, subsection, j)
                 if subsection != pattern:
                     repeats = False
                     break
             
             if repeats:
                 valid = False
-                part1_total += id
-                print(id_str, pattern)
-                break
 
-    # print(r)
+                if 2*len(pattern) == len(id_str):
+                    part1_total += id
+
+                if not counted:
+                    part2_total += id
+                    counted = True
 
 print(part1_total)
+print(part2_total)
