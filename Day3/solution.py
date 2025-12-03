31d9b17f466d35a1b7d6306938cd0cf1e5948d91
Ryan Groch <ryan.w.groch@gmail.com>
2025-12-03 10:36:27 -0600

    Day 3 Part 2 solution

--- Day3/solution.py
+++ Day3/solution.py
@@ -1,16 +1,40 @@
 import sys
 
-total = 0
+total_2 = 0
+total_12 = 0
 
 with open(sys.argv[1], "r") as f:
     for line in f:
         line = line.strip()
-        curr_max = 0
-        for i in range(len(line)):
-            for j in range(i + 1, len(line)):
-                current_pair = str(line[i]) + str(line[j])
-                curr_max = max(curr_max, int(current_pair))
 
-        total += curr_max
+        included = set()
+        number = ["" for _ in line]
+        num_chars = 0
 
-print(total)
+        while num_chars < 12:
+            if num_chars == 2:
+                total_2 += int("".join(number))
+
+            highest_i = 0
+            highest_num = int(line[0])
+            
+            for i in range(1, len(line)):
+                if i in included:
+                    continue
+
+                number[i] = line[i]
+                curr_num = int("".join(number))
+                number[i] = ""
+
+                if curr_num > highest_num:
+                    highest_i = i
+                    highest_num = curr_num
+
+            included.add(highest_i)
+            number[highest_i] = line[highest_i]
+            num_chars += 1
+
+        total_12 += int("".join(number))
+
+print(f"Part 1: {total_2}")
+print(f"Part 2: {total_12}")
