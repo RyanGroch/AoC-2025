0440bd6a19127ab1ba0157b0b7f2f12c98df77fd
Ryan Groch <ryan.w.groch@gmail.com>
2025-12-03 23:34:24 -0600

    Day 4 part 2

--- Day4/solution.py
+++ Day4/solution.py
@@ -1,12 +1,16 @@
 import sys
 
 rows = []
-total = 0
+removed = -1
+total_1 = 0
+total_2 = 0
 
 with open(sys.argv[1], "r") as f:
     for line in f:
         rows.append(list(line.strip()))
 
+while removed != 0:
+    removed = 0
     for r in range(len(rows)):
         for c in range(len(rows[r])):
             if rows[r][c] != '@':
@@ -19,13 +23,21 @@ with open(sys.argv[1], "r") as f:
                     if x < 0 or y < 0 or \
                        x >= len(rows[r]) or \
                        y >= len(rows) or \
-                       (y == r and x == c):
+                       (y == r and x == c) or \
+                       rows[y][x] != '@':
                         continue
 
-                    if rows[y][x] == '@':
-                        adj_count += 1 
+                    adj_count += 1 
 
             if adj_count < 4:
-                total += 1
+                removed += 1
+                if total_1 != 0:
+                    rows[r][c] = "."
 
-print(total)
+    if total_1 == 0:
+        total_1 = removed
+    else:
+        total_2 += removed
+
+print(f"Part 1: {total_1}")
+print(f"Part 2: {total_2}")
