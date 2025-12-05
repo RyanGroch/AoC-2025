5191fb89e15e9f4f1402475fd9293450b02b84dd
Ryan Groch <ryan.w.groch@gmail.com>
2025-12-05 00:26:37 -0600

    Wrong day 5 part 2

--- Day5/solution.py
+++ Day5/solution.py
@@ -2,7 +2,8 @@ import sys
 
 ids = []
 building_set = True
-fresh = 0
+fresh1 = 0
+fresh2 = 0
 
 with open(sys.argv[1], "r") as f:
     for line in f:
@@ -19,11 +20,41 @@ with open(sys.argv[1], "r") as f:
             for low, high in ids:
                 cont = True
                 if check >= low and check <= high:
-                    fresh += 1
+                    fresh1 += 1
                     cont = False
                     break
 
                 if not cont:
                     break
 
-print(fresh)
+
+for i in range(len(ids)):
+    low,high = ids[i]
+    low = int(low)
+    high = int(high)
+    extra_pass = True
+
+    while extra_pass:
+        extra_pass = False
+        for j in range(i):
+            prev_low,prev_high = ids[j]
+            prev_low = int(prev_low)
+            prev_high = int(prev_high)
+
+            if low >= prev_low and low <= prev_high:
+                extra_pass = True
+                low = prev_high + 1
+
+            if high >= prev_low and high <= prev_high:
+                extra_pass = True
+                high = prev_low - 1
+
+    if low <= high:
+        # print("IGNORED: ", ids[i], low, high)
+        fresh2 += (1 + high - low)
+        continue
+
+    # print(ids[i], low,high, (high - low + 1))
+
+print(fresh1)
+print(fresh2)
