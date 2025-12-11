095c40d06034f8c6496550d1fbd53eaf478f060c
Ryan Groch <ryan.w.groch@gmail.com>
2025-12-11 00:03:23 -0600

    Day 11 Part 2

--- Day11/solution.py
+++ Day11/solution.py
@@ -9,16 +9,50 @@ with open(sys.argv[1], "r") as f:
         line = line.split(":")
         adj[line[0]] = line[1].strip().split(" ")
 
-# seen.add("you")
 q.append("you")
-paths = 0
+paths1 = 0
 
 while len(q) > 0:
     loc = q.popleft()
     for dest in adj[loc]:
         if dest == "out":
-            paths += 1
+            paths1 += 1
         else:
             q.append(dest)
 
-print(paths)
+paths2 = 0
+routes = {
+    ("svr", "dac"): 0, 
+    ("svr", "fft"): 0, 
+    ("dac", "fft"): 0,
+    ("fft", "dac"): 0,
+    ("dac", "out"): 0,
+    ("fft", "out"): 0
+}
+
+queued = {}
+
+for start,end in routes.keys():
+    q.append(start)
+    queued[start] = 1
+    while len(q) > 0:
+        loc = q.popleft()
+        paths_to_loc = queued[loc]
+        del queued[loc]
+
+        for dest in adj[loc]:
+            if dest == end:
+                routes[(start,end)] += paths_to_loc
+            elif dest not in ("dac", "fft", "out"):
+                if dest in queued:
+                    queued[dest] += paths_to_loc
+                else:
+                    q.append(dest)
+                    queued[dest] = paths_to_loc
+
+paths2 = \
+    (routes[("svr", "dac")] * routes[("dac", "fft")] * routes[("fft", "out")]) + \
+    (routes[("svr", "fft")] * routes[("fft", "dac")] * routes[("dac", "out")])
+
+print(f"Part 1: {paths1}")
+print(f"Part 2: {paths2}")
